08a3923dd50420209d0c388cebbb8bc6d5f8e893
Hazar <hazar.akbuz@outlook.com>
2018-06-04 16:26:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,6 +17,15 @@ client.on('message', message => {
 			message.author.send('Köpüş sunucusunun topluluk kurallarını kabul ettiğin için teşekkür ederiz.\nArtık sunucudaki diğer sesli odalara ve metin kanallarına erişebilirsin.\n\nSık Kullanılan Bağlantılar:\n\n**»** Arkadaşınla ortak video izleyebilmek için: __http://bit.ly/kopusw2gether__\n\n**»** Sunucu anlık davet bağlantısı: __http://bit.ly/kopusDC__\n\n**»** Müzik botunu kanalına çağırmak için:\n\n__!!!play -müzik bağlantısı veya isim-__\n__!!!c -seçenek-__')
 		}
 	}
+	if (message.content === 'lol') {
+		if (message.channel.id === `452575245052674058`) {
+			if(message.member.roles.has(`453186864422191149`)) {
+				message.member.removeRole(`453186864422191149`);
+				message.author.send('Artık **lol_odası** kanalını göremeyeceksiniz. Görebilmek için tekrar **bot_kanalları** odasına **lol** yazmanız gerekli.')
+			} else {
+				message.member.addRole(`453186864422191149`);
+				message.author.send('Artık **lol_odası** kanalını göremeyeceksiniz. Görebilmek için tekrar **bot_kanalları** odasına **lol** yazmanız gerekli.')
+			}
 });
 
 client.on('guildMemberAdd', async member => {
