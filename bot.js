865560f7ab70122a8437b86804e46a98c187bd2e
Hazar <hazar.akbuz@outlook.com>
2018-06-04 16:37:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
 	if (message.content === '!komutlar') {
-		message.channel.send(`Sunucumuzda kullanabildiğin komutları aşağıda listeledik! :upside_down:\n\n**»** Arkadaşınla ortak video izleyebilmek için: __http://bit.ly/kopusw2gether__\n\n**»** Sunucu anlık davet bağlantısı: __http://bit.ly/kopusDC__\n\n**»** Müzik botunu kanalına çağırmak için:\n\n__!!!play -müzik bağlantısı veya isim-__\n__!!!c -seçenek-__`);
+		message.channel.send(`Sunucumuzda kullanabildiğin komutları aşağıda listeledik! :upside_down:\n\n**»** Arkadaşınla ortak video izleyebilmek için: __http://bit.ly/kopusw2gether__\n\n**»** Sunucu anlık davet bağlantısı: __http://bit.ly/kopusDC__\n\n**»** Müzik botunu kanalına çağırmak için:\n\n__!!!play -müzik bağlantısı veya isim-__\n__!!!c -seçenek-__\n\n**»** Oynadığın oyunla ilgili sohbet kanalına erişim için sohbete şunu yazabilirsin:\n__lol__, __cs__, __pubg__`);
 	}
 	if (message.content === 'köpüş') {
 		if (message.channel.id === `428540697419972609`) {
@@ -22,11 +22,31 @@ client.on('message', message => {
 			if(message.member.roles.has(`453186864422191149`)) {
 				message.member.removeRole(`453186864422191149`);
 				message.author.send('Artık **lol_odası** kanalını göremeyeceksiniz. Görebilmek için tekrar **bot_kanalları** odasına **lol** yazmanız gerekli.')
-				message.delete(120);
 			} else {
 				message.member.addRole(`453186864422191149`);
 				message.author.send('Artık **lol_odası** kanalını görebilirsin. Eğer bu kanalı görmek istemezsen tekrar **bot_kanalları** odasına **lol** yazmanız gerekli.')
-				message.delete(120);
+			}
+		}
+	}
+	if (message.content === 'pubg') {
+		if (message.channel.id === `452575245052674058`) {
+			if(message.member.roles.has(`453190356956348416`)) {
+				message.member.removeRole(`453190356956348416`);
+				message.author.send('Artık **pubg_odası** kanalını göremeyeceksiniz. Görebilmek için tekrar **bot_kanalları** odasına **lol** yazmanız gerekli.')
+			} else {
+				message.member.addRole(`453190356956348416`);
+				message.author.send('Artık **pubg_odası** kanalını görebilirsin. Eğer bu kanalı görmek istemezsen tekrar **bot_kanalları** odasına **lol** yazmanız gerekli.')
+			}
+		}
+	}
+	if (message.content === 'cs') {
+		if (message.channel.id === `452575245052674058`) {
+			if(message.member.roles.has(`453190327252287499`)) {
+				message.member.removeRole(`453190327252287499`);
+				message.author.send('Artık **cs_odası** kanalını göremeyeceksiniz. Görebilmek için tekrar **bot_kanalları** odasına **lol** yazmanız gerekli.')
+			} else {
+				message.member.addRole(`453190327252287499`);
+				message.author.send('Artık **cs_odası** kanalını görebilirsin. Eğer bu kanalı görmek istemezsen tekrar **bot_kanalları** odasına **lol** yazmanız gerekli.')
 			}
 		}
 	}
