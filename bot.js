25d1ae22c992e00a76ec6db6cd7cea63aca27cb6
Hazar <hazar.akbuz@outlook.com>
2018-06-13 02:52:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -61,11 +61,11 @@ client.on('message', message => {
 			if(message.member.roles.has(`453186864422191149`)) {
 				message.member.removeRole(`453186864422191149`);
 				//message.delete(120)
-				message.author.send('\nArtık **lol_odası** kanalını göremeyeceksin. Görebilmek için tekrar **bot_kanalları** odasına **lol** yazmanız gerekli.')
+				message.channel.reply('\nArtık **lol_odası** kanalını göremeyeceksin. Görebilmek için tekrar **bot_kanalları** odasına **lol** yazmanız gerekli.')
 			} else {
 				message.member.addRole(`453186864422191149`);
 				//message.delete(120)
-				message.author.send('\nArtık **lol_odası** kanalını görebilirsin. Eğer kanalı görmek istemezsen tekrar **bot_kanalları** odasına **lol** yazmanız gerekli.')
+				message.channel.reply('\nArtık **lol_odası** kanalını görebilirsin. Eğer kanalı görmek istemezsen tekrar **bot_kanalları** odasına **lol** yazmanız gerekli.')
 			}
 		}
 	}
@@ -73,10 +73,10 @@ client.on('message', message => {
 		if (message.channel.id === `452575245052674058`) {
 			if(message.member.roles.has(`453190356956348416`)) {
 				message.member.removeRole(`453190356956348416`);
-				message.author.send('\nArtık **pubg_odası** kanalını göremeyeceksiniz. Görebilmek için tekrar **bot_kanalları** odasına **pubg** yazmanız gerekli.')
+				message.channel.reply('\nArtık **pubg_odası** kanalını göremeyeceksiniz. Görebilmek için tekrar **bot_kanalları** odasına **pubg** yazmanız gerekli.')
 			} else {
 				message.member.addRole(`453190356956348416`);
-				message.author.send('\nArtık **pubg_odası** kanalını görebilirsin. Eğer kanalı görmek istemezsen tekrar **bot_kanalları** odasına **pubg** yazmanız gerekli.')
+				message.channel.reply('\nArtık **pubg_odası** kanalını görebilirsin. Eğer kanalı görmek istemezsen tekrar **bot_kanalları** odasına **pubg** yazmanız gerekli.')
 			}
 		}
 	}
@@ -84,10 +84,10 @@ client.on('message', message => {
 		if (message.channel.id === `452575245052674058`) {
 			if(message.member.roles.has(`453190327252287499`)) {
 				message.member.removeRole(`453190327252287499`);
-				message.author.send('\nArtık **cs_odası** kanalını göremeyeceksiniz. Görebilmek için tekrar **bot_kanalları** odasına **cs** yazmanız gerekli.')
+				message.channel.reply('\nArtık **cs_odası** kanalını göremeyeceksiniz. Görebilmek için tekrar **bot_kanalları** odasına **cs** yazmanız gerekli.')
 			} else {
 				message.member.addRole(`453190327252287499`);
-				message.author.send('\nArtık **cs_odası** kanalını görebilirsin. Eğer kanalı görmek istemezsen tekrar **bot_kanalları** odasına **cs** yazmanız gerekli.')
+				message.channel.reply('\nArtık **cs_odası** kanalını görebilirsin. Eğer kanalı görmek istemezsen tekrar **bot_kanalları** odasına **cs** yazmanız gerekli.')
 			}
 		}
 	}
@@ -95,10 +95,10 @@ client.on('message', message => {
 		if (message.channel.id === `452575245052674058`) {
 			if(message.member.roles.has(`453935578824835103`)) {
 				message.member.removeRole(`453935578824835103`);
-				message.author.send('\nArtık **oyun_diğer** kanalını göremeyeceksiniz. Görebilmek için tekrar **bot_kanalları** odasına **diğer** yazmanız gerekli.')
+				message.channel.reply('\nArtık **oyun_diğer** kanalını göremeyeceksiniz. Görebilmek için tekrar **bot_kanalları** odasına **diğer** yazmanız gerekli.')
 			} else {
 				message.member.addRole(`453935578824835103`);
-				message.author.send('\nArtık **oyun_diğer** kanalını görebilirsin. Eğer kanalı görmek istemezsen tekrar **bot_kanalları** odasına **diğer** yazmanız gerekli.')
+				message.channel.reply('\nArtık **oyun_diğer** kanalını görebilirsin. Eğer kanalı görmek istemezsen tekrar **bot_kanalları** odasına **diğer** yazmanız gerekli.')
 			}
 		}
 	}
