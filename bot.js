07622574eef2cd8249f19566108c2c04b7b5e58e
Hazar <hazar.akbuz@outlook.com>
2018-06-13 11:55:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,8 +25,8 @@ client.on('message', message => {
 	if (message.content === 'köpüş') {
 		if (message.channel.id === `428540697419972609`) {
 			message.member.removeRole(`428536643042017284`);
-			message.react("✅")
-			message.delete(120)
+			message.react("✅");
+			message.delete(120);
 			//message.author.send('Köpüş sunucusunun topluluk kurallarını kabul ettiğin için teşekkür ederiz.\nArtık sunucudaki diğer sesli odalara ve metin kanallarına erişebilirsin.\n\nSık Kullanılan Bağlantılar:\n\n**»** Arkadaşınla ortak video izleyebilmek için: __http://bit.ly/kopusw2gether__\n\n**»** Sunucu anlık davet bağlantısı: __http://bit.ly/kopusDC__\n\n**»** Müzik botunu kanalına çağırmak için:\n\n__!!!play -müzik bağlantısı veya isim-__\n__!!!c -seçenek-__')
 			var embed = new Discord.RichEmbed()
 			.setColor('#f8f808')
